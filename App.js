0eb6d48273e19d8d4382415263c7f1f6b1265dbd
SamiUllah-dev336 <samidev336@gmail.com>
2024-03-19 22:51:41 +0500

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Image} from 'react-native';
+import normalize from 'react-native-normalize';
+import Textbox from './Component/textbox';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.outerV}>
+        <Image
+          style={styles.tinyLogo}
+          source={require('./assets/favicon.png')}
+        />
+      </View>
+      
+      <Textbox iconName='email' placeholder='Enter email' Ktype='email-address' />
+      <Textbox iconName='email' placeholder='Enter email' Ktype='email-address' />
+      <Text style={{fontSize:normalize(40)}}>Here is a text!</Text>
     </View>
   );
 }
@@ -15,6 +25,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  outerV:{
+    marginTop:normalize(100,'height'),
+    width:normalize(250),
+    height:normalize(250,'height'),
+    backgroundColor:'yellow',
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  tinyLogo:{
+    width:normalize(200),
+    height:normalize(200,'height'),
+  }
 });
---
+++ Component/textbox.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View ,Image, TextInput,KeyboardAvoidingView} from 'react-native';
+import normalize from 'react-native-normalize';
+import Icon from 'react-native-vector-icons/Zocial'
+
+export default function Textbox(props) {
+  return (
+    <View style={styles.container}>
+        <Icon name={props.iconName} size={normalize(30)} style={styles.icon} />
+        <TextInput 
+          placeholder={props.placeholder}
+          keyboardType={props.Ktype}
+        />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'pink',
+    marginTop:normalize(20),
+    width:'86%',
+    height:'8%',
+    borderRadius:normalize(10) ,
+    flexDirection:'row',
+    alignItems:'center',
+  },
+  icon:{
+    marginLeft:normalize(10),
+    marginRight:normalize(10)
+  }
+
+});
